653b8a9330fe0069795aefbcc92554d2e6163434
Ankit Soni <ankisoni453@gmail.com>
2022-06-21 10:04:54 +0530

    first commit
    first commit

---
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.example.payroll;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/employee")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) {
+
+        return repository.findById(id).orElseThrow(
+                () -> new EmployeeNotFoundException(id));
+    }
+
+
+}
---
+++ src/main/java/com/example/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee" + id);
+    }
+
+}
